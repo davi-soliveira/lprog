08af916e1b3542d5698513b54083a56722599b6e
Davisolivera <111003553+davixexeno@users.noreply.github.com>
2022-08-22 17:16:50 +0000

    aula2

--- aula 2/main.cs
+++ aula 2/main.cs
@@ -1,7 +1,30 @@
 using System;
 
 class aula2{
+  public struct Aluno{
+    public string matricula;
+    public int periodo;
+  }
+  public struct Disciplina {
+    public string nome;
+    public int semestre;
+  }
+  public static bool Matricular(
+    Aluno aluno,
+    Disciplina disciplina
+    ){
+    return aluno.periodo == disciplina.semestre;
+  }
   public static void Main(string[] args){
-    
+    Aluno aluno = new Aluno{
+      matricula = "0",
+      periodo = 1
+    };
+    Disciplina disciplina = new Disciplina{
+      nome="programação",
+      Semestre = 3
+    };
+    bool matricular = Matricular(aluno,disciplina);
+    Console.WriteLine(matricular);
   }
 }
