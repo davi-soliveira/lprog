caf011bf15c01ba5784adfc655a859b40858faf4
Davisolivera <111003553+davixexeno@users.noreply.github.com>
2022-09-22 19:44:19 +0000

    Modelos,aula6,aula7

---
+++ modelos/Professor.cs
@@ -0,0 +1,16 @@
+namespace Modelos {
+
+  class Professor : Pessoa {
+
+    public string Codigo {
+      get; set;
+    }
+
+    public override bool Validar() {
+      return this.Codigo != null &&
+        this.Codigo.Length > 0;
+    }
+
+  }
+
+}
